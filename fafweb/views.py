302983d3a2f73a1bdd32f9e99954678b08f7b9d0
Grenbur <hortros@gmail.com>
2017-01-18 20:19:55 +0100

    debug commit

--- fafweb/views.py
+++ fafweb/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
+    if (request.is_secure()):
+        redirect(request.build_absolute_uri().replace('https', 'http'))
     return render(request, 'home.html')
 
 def redir(request, url):
